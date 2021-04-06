e1614871c90827b998b4a1bee486bdfb5e8b8bd5
sminy85 <smini857@naver.com>
2021-04-06 11:29:13 +0900

    redeem main page

--- screens/product.js
+++ screens/product.js
@@ -1,9 +1,19 @@
 import React, { useEffect } from "react";
-import {Text, View, ActivityIndicator } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import {
+    Text,
+    View,
+    ActivityIndicator,
+    StyleSheet,
+    Image,
+    Alert,
+    TouchableOpacity,
+    ScrollView
+} from "react-native";
 import axios from "axios";
 import { API_URL } from "../config/constants";
 import {useState} from "react";
+import Avatar from "../assets/icons/avatar.png";
+import dayjs from "dayjs";
 
 export default function ProductScreen(props){
     const {id} = props.route.params;
@@ -18,14 +28,104 @@ export default function ProductScreen(props){
         })
     }, []);
 
+    const onPressButton = () => {
+        Alert.alert('구매가 완료되었습니다.');
+    }
+
     if(!product) {
         return <ActivityIndicator />
     }
 
     return (
-        <ScrollView>
-        <Text>상품 상세 화면</Text>
-        <Text>{product.name}</Text>
-        </ScrollView>
+        <View style={styles.container}>
+            <ScrollView>
+                <View>
+                    <Image
+                        style={styles.productImage}
+                        source={{uri: `${API_URL}/${product.imageUrl}`}}
+                        resizeMode="contain"/>
+                </View>
+                <View style={styles.productSection}>
+                    <View style={styles.productSeller}>
+                        <Image
+                            style={styles.avatarImage}
+                            source={Avatar} />
+                            <Text>{product.seller}</Text>
+                    </View>
+                    <View style={styles.divider} />
+                    <View>
+                        <Text style={styles.productName} >{product.name}</Text>
+                        <Text style={styles.productPrice} >{product.price}원</Text>
+                        <Text style={styles.productDate}>
+                            {dayjs(product.createdAt).format('YYYY년 MM월 DD일')}</Text>
+                        <Text style={styles.productDescription}>{product.description}</Text>
+                    </View>
+                </View>
+            </ScrollView>
+            <TouchableOpacity onPress={onPressButton}>
+                <View style={styles.purchaseButton}>
+                    <Text style={styles.purchaseText}>구매하기</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    productImage: {
+        width: '100%',
+        height: 300,
+    },
+    avatarImage: {
+        width: 50,
+        height: 50,
+    },
+    productSeller: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    productSection: {
+        padding: 8,
+    },
+    divider: {
+        backgroundColor: '#e9ecef',
+        height: 1,
+        marginVertical: 16,
+    },
+    productName: {
+        fontSize: 20,
+        fontWeight: 400,
+    },
+    productPrice: {
+        fontSize: 18,
+        fontWeight: 700,
+        marginTop: 8,
+    },
+    productDate: {
+        fontSize: 14,
+        marginTop: 4,
+        color: 'rgb(204,204,204)',
+    },
+    productDescription: {
+        marginTop: 16,
+        fontSize: 17,
+    },
+    purchaseButton: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: 60,
+        backgroundColor: 'rgb(255, 80, 88)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    purchaseText: {
+        color: 'white',
+        fontSize: 20,
+    }
+});
